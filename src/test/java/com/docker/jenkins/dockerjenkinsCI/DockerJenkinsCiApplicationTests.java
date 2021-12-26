cdbb29420dcf5046700a8c84041bc00be3039e35
SK Aaftab Alam Quaderi <aaftabalam@SKs-MacBook-Air.local>
2021-12-26 21:24:18 +0530

    first commint

---
+++ src/test/java/com/docker/jenkins/dockerjenkinsCI/DockerJenkinsCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkins.dockerjenkinsCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
